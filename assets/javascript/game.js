d9ad1443f8a5835c131820c869ede1b85efb70f8
dkwazhere <dhealth77@gmail.com>
2017-12-16 20:12:19 -0800

    added audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,13 @@ $(document).ready(function(){
     var number2 = Math.floor((Math.random() *11)+1);
     var number3 = Math.floor((Math.random() *11)+1);
     var number4 = Math.floor((Math.random() *11)+1);
+    //audios
+    var winAudio = new Audio("assets/sounds/tubbyagain.wav");
+    var loseAudio = new Audio("assets/sounds/tubbyuhoh.wav");
+    var tinkyAudio = new Audio("assets/sounds/tubbytinky.wav");
+    var dipsyAudio = new Audio("assets/sounds/tubbydipsy.wav");
+    var laalaaAudio = new Audio("assets/sounds/tubbylaalaa.wav");
+    var poAudio = new Audio("assets/sounds/tubbypo.wav");
     // dom manipulation
     $("#randomnumber").text("Target # " + randomNumber);
     $("#usernumber").text("Your # " + userNumber);
@@ -29,12 +36,22 @@ function reset(){
 function winOrLose (){
     if (userNumber == randomNumber) {
         wins++;
+        tinkyAudio.pause();
+        dipsyAudio.pause();
+        laalaaAudio.pause();
+        poAudio.pause();
+        winAudio.play();
         alert("you win");
         $("#win").text("Your wins: " + wins);
         reset();
     }
     else if (userNumber > randomNumber) {
         loss++;
+        tinkyAudio.pause();
+        dipsyAudio.pause();
+        laalaaAudio.pause();
+        poAudio.pause();
+        loseAudio.play();
         alert("you lose");
         $("#loss").text("Your losses " + loss);
         reset();
@@ -44,24 +61,28 @@ function winOrLose (){
 //clicking each character
 $("#dipsy").on("click", function() {
     userNumber = userNumber + number1;
+    dipsyAudio.play();
     console.log(userNumber);
     $("#usernumber").text("Your # " + userNumber);
     winOrLose();
 });
 $("#laa-laa").on("click", function() {
     userNumber = userNumber + number2;
+    laalaaAudio.play();
     console.log(userNumber);
     $("#usernumber").text("Your # " + userNumber);
     winOrLose();
 });
 $("#po").on("click", function() {
     userNumber = userNumber + number3;
+    poAudio.play();
     console.log(userNumber);
     $("#usernumber").text("Your # " + userNumber);
     winOrLose();
 });
 $("#tinky").on("click", function() {
     userNumber = userNumber + number4;
+    tinkyAudio.play();
     console.log(userNumber);
     $("#usernumber").text("Your # " + userNumber);
     winOrLose();
